3a2d210da4c944aea8ed116e71e7df8d4435f0d0
Andras Wirth <agrippa.wia@gmail.com>
2018-10-16 12:35:56 +0200

    cucumber automated testing possibility added

---
+++ src/main/resources/cucumber/navigation.feature
@@ -0,0 +1,10 @@
+@tag
+Feature: Menu
+  I want to use this template to check the Menu structure is accessible
+
+  @tag1
+  Scenario: Home page
+    Given Open the application
+    Then The Home button is available
+    And The Generate button is available
+    
---
+++ src/test/java/hu/wirthandras/shifts/cucumber/NavigationCucumberTest.java
@@ -0,0 +1,60 @@
+package hu.wirthandras.shifts.cucumber;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Gecko Driver is required for execution 
+ */
+@RunWith(Cucumber.class)
+//TODO find the correct solution
+@Ignore("Restrict to run with JUnit")
+public class NavigationCucumberTest {
+
+	@Autowired
+	private static WebDriver driver;
+	
+	@Before
+	public static void setUp() {
+		driver = new FirefoxDriver();
+	} 
+
+	@Given("^Open the application$")
+	public void open_the_application() throws Throwable {
+		driver.get("http://localhost:9000");
+	}
+
+	@Then("^The Home button is available$")
+	public void the_Home_button_is_available() throws Throwable {		
+		WebElement findElement = driver.findElement(By.id("home"));
+		assertThat(true, is(findElement.isDisplayed()));
+	}
+
+	@Then("^The Generate button is available$")
+	public void the_Generate_button_is_available() throws Throwable {
+		WebElement findElement = driver.findElement(By.id("generate"));
+		assertThat(true, is(findElement.isDisplayed()));
+	}
+
+	@After
+	public static void tearDown() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
+}
