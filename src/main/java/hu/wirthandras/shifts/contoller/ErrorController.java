8dda8c26e9359b3f44c7fbeb66273983dd9263c8
Andras Wirth <agrippa.wia@gmail.com>
2018-11-04 23:31:29 +0100

    TODO added to ErrorController

--- src/main/java/hu/wirthandras/shifts/contoller/ErrorController.java
+++ src/main/java/hu/wirthandras/shifts/contoller/ErrorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorController {
 
+	//TODO * not handle all possibilities
 	@GetMapping("*")
 	public String error() {
 		//TODO improve to handle any type of possible errors 
