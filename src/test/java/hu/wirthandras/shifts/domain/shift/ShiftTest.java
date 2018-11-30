2ec57388fcff20e2e67a2ee91e1f2316613a25a6
Andras Wirth <agrippa.wia@gmail.com>
2018-11-30 10:22:04 +0100

    toInterval Unit tests added

--- src/test/java/hu/wirthandras/shifts/domain/shift/ShiftTest.java
+++ src/test/java/hu/wirthandras/shifts/domain/shift/ShiftTest.java
@@ -1,21 +1,39 @@
 package hu.wirthandras.shifts.domain.shift;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import hu.wirthandras.shifts.domain.Shift;
 
 public class ShiftTest {
 
+	private Shift sNight;
+	private Shift sShort;
+
+	@Before
+	public void setUp() {
+		sNight = new Shift(null, 18, 6, null);
+		sShort = new Shift(null, 8, 15, null);
+	}
+
 	@Test
 	public void testShiftDurationShouldBe12Hour() {
-		Shift shift = new Shift(null, 18, 6, null);
-		Assert.assertEquals(12, shift.duration());
+		Assert.assertEquals(12, sNight.duration());
 	}
 
 	@Test
 	public void testShiftIsNightShouldBe7Hour() {
-		Shift shift = new Shift(null, 8, 15, null);
-		Assert.assertEquals(7, shift.duration());
+		Assert.assertEquals(7, sShort.duration());
+	}
+
+	@Test
+	public void testToIntervalShouldBe8H15H() {
+		Assert.assertEquals("8-15", sShort.toInterval());
+	}
+
+	@Test
+	public void testToIntervalShouldBe18H6H() {
+		Assert.assertEquals("18-6", sNight.toInterval());
 	}
 }
