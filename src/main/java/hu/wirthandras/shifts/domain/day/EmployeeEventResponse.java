6807fcc05ac67cb6ba4439b5655a9885164144a0
Andras Wirth <agrippa.wia@gmail.com>
2018-12-04 14:07:54 +0100

    Formatting corrected

--- src/main/java/hu/wirthandras/shifts/domain/day/EmployeeEventResponse.java
+++ src/main/java/hu/wirthandras/shifts/domain/day/EmployeeEventResponse.java
@@ -5,25 +5,24 @@ import java.util.List;
 import hu.wirthandras.shifts.domain.employee.EmployeeEvent;
 
 public class EmployeeEventResponse {
-	
-    private List<EmployeeEvent> result;
-    
-    public EmployeeEventResponse() {
-    	super();
-    }
-    
-    public EmployeeEventResponse(List<EmployeeEvent> result) {
-    	super();
-    	this.result = result;
-    }
-    
+
+	private List<EmployeeEvent> result;
+
+	public EmployeeEventResponse() {
+		super();
+	}
+
+	public EmployeeEventResponse(List<EmployeeEvent> result) {
+		super();
+		this.result = result;
+	}
+
 	public List<EmployeeEvent> getResult() {
 		return result;
 	}
-	
+
 	public void setResult(List<EmployeeEvent> result) {
 		this.result = result;
 	}
-	
 
 }
