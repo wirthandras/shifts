987dc4bed47386d2c5203e983863b957e8cc3f10
Andras Wirth <agrippa.wia@gmail.com>
2019-02-18 17:20:40 +0100

    use @GetMapping instead of @RequestMapping on IndexController

--- src/main/java/hu/wirthandras/shifts/contoller/IndexController.java
+++ src/main/java/hu/wirthandras/shifts/contoller/IndexController.java
@@ -1,12 +1,12 @@
 package hu.wirthandras.shifts.contoller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
